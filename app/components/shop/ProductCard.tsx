c4a98a38c84ec2b307abb5f65068bb5043599945
Lauther99 <lautherharold27@gmail.com>
2026-01-20 18:40:32 -0500

    added currencies

--- app/components/shop/ProductCard.tsx
+++ app/components/shop/ProductCard.tsx
@@ -1,6 +1,14 @@
 import { Product } from "@/types/product";
 import Image from "next/image";
 
+type CurrencyCode = "SOL" | "MXN" | "USD" | any;
+
+const currencies: Record<CurrencyCode, string> = {
+  SOL: "S/",
+  MXN: "$",
+  USD: "$"
+};
+
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="shop-card">
@@ -16,7 +24,7 @@ export default function ProductCard({ product }: { product: Product }) {
       <div className="shop-info">
         <div className="brand-label">{product.brand}</div>
         <div className="shop-title">{product.title}</div>
-        <div className="shop-price">${product.price.toFixed(2)}</div>
+        <div className="shop-price">{currencies[product.currency]}{product.price.toFixed(2)}</div>
         <button className="btn-add-cart">Añadir al Carrito</button>
       </div>
     </div>
