9770d9207037c4e9040b005573b4322bf8ad0882
Lauther27 <lautherharold27@gmail.com>
2026-02-02 17:30:15 -0500

    added product images

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,30 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["m.media-amazon.com", "images.unsplash.com"],
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "m.media-amazon.com",
+      "caraudioexpress.pe",
+      "www.apoloaudio.com.pe",
+      "autoradio.pe",
+      "media.falabella.com",
+      "s.alicdn.com",
+      "resources-sami4.s3.us-west-1.amazonaws.com",
+      "http2.mlstatic.com",
+      "autopartes.safari.com.pe",
+      "oechsle.vteximg.com.br",
+      "i.ebayimg.com",
+      "www.toyotaperu.com.pe",
+      "ae01.alicdn.com",
+      "denniscarsac.com",
+      "apr.pe",
+      "pvljogumgjnzfsbjpyke.supabase.co",
+      "cdnx.jumpseller.com",
+      "image.made-in-china.com",
+      "tecgo.com.pe",
+    ],
     remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
 };
